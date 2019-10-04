aaa4f79efe9cfcce9b8464b5c3cb20e5ec3986a1
Juan C. Cuao <juancamilocuao@gmail.com>
2019-10-04 11:34:49 -0500

    Adicionando CORS

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 """Flask App Project."""
 
 from flask import Flask, jsonify
+from flask_cors import cross_origin
 app = Flask(__name__)
 
 
 @app.route('/')
+@cross_origin()
 def index():
     """Return homepage."""
     json_data = {'Hello': 'World!'}
